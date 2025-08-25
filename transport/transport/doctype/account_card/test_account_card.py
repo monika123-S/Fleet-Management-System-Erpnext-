9ad02b0dc6fa8afe531647d7062e977b1956e683
monika123-S <monikarajan1020@gmail.com>
2025-08-25 12:52:43 +0530

    Remove account_card doctype files

--- transport/transport/doctype/account_card/test_account_card.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Monika and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestAccount_card(FrappeTestCase):
-	pass
